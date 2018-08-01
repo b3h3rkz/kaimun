aa6243da009ae8680bd01f36b11a253b4fb80f1e
Bernard Parah <barjebernard@gmail.com>
2018-08-01 23:58:50 +0100

    send payment

--- transaction/money_wave_utils.py
+++ transaction/money_wave_utils.py
@@ -3,7 +3,8 @@ Set of utility methods for use with the moneywave api
 """
 
 import requests
-
+from random import randint
+import os
 
 BASE_URL = "https://moneywave.herokuapp.com/v1/"
 auth_token = ''
@@ -11,10 +12,11 @@ auth_token = ''
 
 def get_auth_token():
     url = BASE_URL + "merchant/verify"
+    # payload = {'apiKey': os.environ['MONEY_RAVE_API_KEY'], 'secret': os.environ['MONEY_RAVE_API_SECRET']}
     payload = {'apiKey': 'ts_JIGRALK6O6YGH196OGPM', 'secret': 'ts_SHU80QZ8NAO2H2ZZPIFHYQD4WQZGMA'}
     headers = {'content-type': 'application/json'}
-    response = requests.request("POST", url, json=payload, headers=headers)
-    response = response.json()
+    response = requests.request("POST", url, json=payload, headers=headers).json()
+    print(response)
     auth_token = response['token']
     return auth_token
 
@@ -23,28 +25,49 @@ def get_banks(country):
     url = BASE_URL + "banks"
     headers = {'content-type': 'application/json'}
     params = {'country': country}
-    response = requests.post(url, params=params, headers=headers)
-    banks = response.json()
-    return banks
+    response = requests.post(url, params=params, headers=headers).json()
+    return response
 
 
 def resolve_account(account_number, bank_code, currency):
     token = get_auth_token()
     headers = {'content-type': 'application/json', 'authorization': token}
     url = BASE_URL + "resolve/account"
-    payload = {'currency': currency, 'bank_code': bank_code, 'account_number': account_number}
-    print(type(payload['account_number']))
-    response = requests.post(url, json=payload, headers=headers)
-    response = response.json()
-    print(response)
+    payload = {
+        'currency': currency,
+        'bank_code': bank_code,
+        'account_number': account_number
+    }
+    # print(type(payload['account_number']))
+    response = requests.post(url, json=payload, headers=headers).json()
     if response['status'] == "success":
-        return True
+        return response
     else:
         return False
 
 
-def disburse(account_number):
-    pass
+def disburse(account_number, bankcode, amount, narration, currency, sender):
+    token = get_auth_token()
+    headers = {'content-type': 'application/json', 'authorization': token}
+    url = BASE_URL + "disburse"
+    payload = {
+        "accountNumber": account_number,
+        "bankcode": bankcode,
+        "amount": amount,
+        "narration": narration,
+        "ref": get_ref(),
+        "currency": currency,
+        "senderName": sender,
+        "lock": "00000000"
+    }
+    response = requests.post(url, json=payload, headers=headers).json()
+    print(response)
+    if response['status'] == "success":
+        print(response, "paid")
+        return response
+    else:
+        return False
 
 
-
+def get_ref():
+    return "Kaimun" + str(randint(00000, 99999))
--- transaction/views.py
+++ transaction/views.py
@@ -1,3 +1,6 @@
+import hashlib
+import random
+from string import ascii_uppercase, digits
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
@@ -6,6 +9,7 @@ from .models import Transaction
 from rest_framework.decorators import list_route, detail_route
 from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny
 from .money_wave_utils import *
+import json
 
 
 class TransactionModelViewSet(ModelViewSet):
@@ -34,15 +38,85 @@ class TransactionModelViewSet(ModelViewSet):
         :param request:
         :return:
         """
-        print(request.data)
         bank_code = request.data['bank_code']
         account_number = request.data['account_number']
         currency = request.data['country']
         resolve = resolve_account(account_number, bank_code, currency)
-        if resolve:
-            return Response({"account": resolve}, status=status.HTTP_200_OK)
-        else:
+        if not resolve:
             return Response(status=status.HTTP_404_NOT_FOUND)
+        else:
+            print(resolve)
+            return Response({"account": resolve}, status=status.HTTP_200_OK)
 
+
+
+    @list_route(methods=['post'])
+    def ravepayment_request(self, request):
+        hashedPayload = ''
+        payload = {
+            "PBFPubKey": 'FLWPUBK-3d15ffa5c7ec93883c651c0f441bc2d0-X',
+            "amount": request.data['amount'],
+            "payment_method": "both",
+            "custom_description": "Kaimun",
+            "custom_title": "Instant Money Transfers",
+            "country": request.data['country'],
+            "currency": request.data['currency'],
+            "customer_email": request.user.email,
+            "customer_firstname": request.user.first_name,
+            "customer_lastname": request.user.last_name,
+            # "customer_phone": request.data['phone'],
+            "txref": "KMN-" + ''.join(random.sample((ascii_uppercase+digits), 5))
+        }
+
+    # sort payload and concatenate into a single string
+        sorted_payload = sorted(payload)
+
+    # concatenate sorted_payload. The payload is rearranged and the values concatenated in the order of the sorted keys.
+        hashed_payload = ''
+        for value in sorted_payload:
+            hashed_payload += value
+        hashed_string = hashed_payload + "FLWSECK-b86e4802fc5eaa03db5e7f73fdc4514e-X"
+        integrity_hash = hashlib.sha256(hashed_string.lower().encode()).hexdigest()
+        return Response({'payload': payload, 'integrityHash': integrity_hash})
+
+
+
+    @list_route(methods=['post'])
+    def ravepay_deposit(self, request):
+        # instance = self.get_object()
+        url = "https://ravesandboxapi.flutterwave.com/flwv3-pug/getpaidx/api/xrequery"
+        data = {
+            "txref": request.data['txRef'],
+            "SECKEY": "FLWSECK-b86e4802fc5eaa03db5e7f73fdc4514e-X",
+            "include_payment_entity": 1
+        }
+        response = requests.post(url, data=data).json()
+
+        account_number = request.data['account_number']
+        bank_code = request.data['bank_code']
+        currency = request.data['currency']
+        amount = float(request.data['amount'])
+        narration = request.data['narration']
+        sender = request.user.first_name + ' ' + request.user.last_name
+
+        # confirm that the response for the transaction is successful
+        if response['status'] == 'success':
+            # confirm that the amount for that transaction is the amount you wanted to charge
+            data = response['data']
+            # print(data[0]['chargedamount'])
+            if data[0]['chargecode'] == '00':
+                chargedamount = float(data[0]['chargedamount'])
+                if chargedamount > amount:
+                        make_transfer = disburse(account_number, bank_code, amount, narration, currency, sender)
+                        if make_transfer:
+                            return Response({'message': 'Successfully Sent Funds'}, status=status.HTTP_200_OK)
+                        else:
+                            return Response({'message': '1Unable  to send funds'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                else:
+                    return Response({'message': '2Unable  to send funds'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            else:
+                return Response({'message': 'Transaction was not successful'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            return Response({'message': '4Unable  to send funds'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
