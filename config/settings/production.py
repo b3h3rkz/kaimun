73a91f5c5e6b1d4761745db458a974971d8d6e5e
Bernard Parah <barjebernard@gmail.com>
2018-08-03 00:38:38 +0100

    updated config db

--- config/settings/production.py
+++ config/settings/production.py
@@ -10,9 +10,9 @@ ALLOWED_HOSTS = ['api.kaimun.com']
 
 # DATABASES
 # ------------------------------------------------------------------------------
-DATABASES['default'] = env.db('DATABASE_URL')  # noqa F405
-DATABASES['default']['ATOMIC_REQUESTS'] = True  # noqa F405
-DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)  # noqa F405
+# DATABASES['default'] = env.db('DATABASE_URL')  # noqa F405
+# DATABASES['default']['ATOMIC_REQUESTS'] = True  # noqa F405
+# DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)  # noqa F405
 
 # CACHES
 # ------------------------------------------------------------------------------
